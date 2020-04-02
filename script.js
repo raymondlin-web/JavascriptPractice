e6a62758554d56fa7a2292193587ac6b39d15f63
raymondlin-web <ryl262@nyu.edu>
2020-04-02 01:19:21 -0400

    JSP

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Welcome!")
+var A = "Boop";
+var B = window.prompt("Guess the word?");
+if A = B;
+  return "Great Job!";
+  else {
+    alert("Try try again");
+    alert("Hint: It's a sound a computer would make");
+  }
+var X = 7;
+var Y = window.prompt("Now guess a number?");
+if X == Y;
+  return "Wow that's amazing!";
+  else if (X > Y) {
+    return "Try again, you're a bit low";
+  }
+  else if (X < Y) {
+    return "Try again, you're a bit high";
+  }
+  else {
+    return "That's not a number :(";
+  }
+function NameTime(Name1, Name2) {
+  console.log("So you are " + (Name1, Name2) + "?");
+  console.log("Wonderful!!");
+}
+function Quarantine(Sick) {
+  if Sick = "Yes";
+    console.log("Oh no please stay 6ft away from me and wash your hands!");
+    else if (Sick = "No") {
+      console.log("Oh thank goodness!");
+    }
+    else {
+      console.log("Well you sound sick.");
+    }
+}
+var GameTime = ["Animal Crossing", "League of Legends", "Resident Evil", "Jumanji"];
+console.log("Should we play " + GameTime.random() +" or " + GameTime.random() + "?");
